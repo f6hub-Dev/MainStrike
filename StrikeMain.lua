946cd2e331375ca0ce5afd89d3fffb34927d3de6
StrikeX <githubidk@gmail.com>
2025-07-25 23:43:29 +0300

    Update StrikeMain.lua

--- StrikeMain.lua
+++ StrikeMain.lua
@@ -1,4 +1,7 @@
-if game.PlaceID ==126884695634066
+if game.PlaceID == 126884695634066
    loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/GaGv3/refs/heads/main/Grow%20a%20Garden.lua"))()
 end
 
+if game.PlaceID == 95508886069297
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/Slap-Tower/refs/heads/main/Slap%20tower.lua"))()
+end
