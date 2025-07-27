e2e4c1b8b5306a3bd66c963908e9de57fb60069d
StrikeX <githubidk@gmail.com>
2025-07-27 19:18:51 +0300

    Update StrikeMain.lua

--- StrikeMain.lua
+++ StrikeMain.lua
@@ -5,3 +5,7 @@ end
 if (game.PlaceId == 95508886069297) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/Slap-Tower/refs/heads/main/Slap%20tower.lua"))()
 end
+
+if (game.PlaceId == 126509999114328) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/99-Nights-in-the-forest/refs/heads/main/StrikeX.lua"))()
+end
