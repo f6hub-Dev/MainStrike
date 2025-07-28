cb978389077905ab3fd81f33fb7d45a19cbf4a4e
StrikeX <githubidk@gmail.com>
2025-07-28 19:13:15 +0300

    Update StrikeMain.lua

--- StrikeMain.lua
+++ StrikeMain.lua
@@ -1,11 +1,15 @@
-if (game.PlaceId == 126884695634066) then
+if (game.PlaceId == 126884695634066) then ------- Grow A Garden
     loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/GaGv3/refs/heads/main/Grow%20a%20Garden.lua"))()
 end
 
-if (game.PlaceId == 95508886069297) then
+if (game.PlaceId == 95508886069297) then ------ Slap Tower
     loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/Slap-Tower/refs/heads/main/Slap%20tower.lua"))()
 end
 
-if (game.PlaceId == 126509999114328) then
+if (game.PlaceId == 126509999114328) then ----- 99 Days in The Forest
     loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/99-Nights-in-the-forest/refs/heads/main/StrikeX.lua"))()
+end
+
+if (game.PlaceId == 109983668079237 then ----- Steal a Brainrot
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/Steal-a-brainrot/refs/heads/main/StrikeX.lua"))()
 end
