16116ac14784f33a353622dcd5bf1928a46f074b
StrikeX <githubidk@gmail.com>
2025-08-16 16:05:12 +0300

    Update StrikeMain.lua

--- StrikeMain.lua
+++ StrikeMain.lua
@@ -2,10 +2,6 @@ if (game.PlaceId == 126884695634066) then ------- Grow A Garden
     loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/GrowAGarden/refs/heads/main/Grow%20a%20Garden.lua"))()
 end
 
-if (game.PlaceId == 95508886069297) then ------ Slap Tower
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/Slap-Tower/refs/heads/main/Slap%20tower.lua"))()
-end
-
 if (game.PlaceId == 126509999114328) then ----- 99 Days in The Forest
     loadstring(game:HttpGet("https://raw.githubusercontent.com/f6hub-Dev/99-Nights-in-the-forest/refs/heads/main/StrikeX.lua"))()
 end
